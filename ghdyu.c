5bb211353aa8d11ba3ba7f060e3c69e8084043e5
gk007kannan <35525377+gk007kannan@users.noreply.github.com>
2018-03-12 03:24:35 -0700

    Create ghdyu.c

---
+++ ghdyu.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+int i,a;
+clrscr();
+printf("\n enter the value of a");
+scanf("\n%d",&a);
+for(i=0;i<=a;i++)
+{
+	printf("\n the number of i is==> %d",i);
+}
+getch();
+}
